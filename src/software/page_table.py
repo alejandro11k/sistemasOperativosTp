a8294ceb1f79faee48f15f1e628e1c16a223ab2b
vicfabrice <discosdevictoria@gmail.com>
2015-11-29 17:08:37 -0300

    comenzando con page fault (que no sé que tan necesario es)

--- src/software/page_table.py
+++ src/software/page_table.py
@@ -7,12 +7,17 @@ class PageTable:
     
     def put (self, adress):
         if self.isStored(adress):
-            self.pageTable[adress] = 1
-        else: self.pageTable[adress] = 0   
+            self.pageTable[adress] = 1 #flag que indica que sí está guardada en mem física
+        else: 
+            self.pageTable[adress] = 0 #flag que indica que no está guardada en mem física FALLO DE PÁG?
+            self.pageFault(adress)
         
+    def pageFault(self, adress):
+        pass
         
     def isStored (self, adress):
         #if it's in real memory or not
         pass
+    
     
         
