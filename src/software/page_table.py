3d98bcc2c4e1d30d5d819c3d6da812fc646ef089
vicfabrice <discosdevictoria@gmail.com>
2015-11-29 16:47:30 -0300

    avanzando con pageTable

--- src/software/page_table.py
+++ src/software/page_table.py
@@ -1,7 +1,18 @@
 class PageTable:
     
+    #se encarga de traducir dirs virtuales en dirs físicas
+    
     def __init__(self):
         self.pageTable = {}
+    
+    def put (self, adress):
+        if self.isStored(adress):
+            self.pageTable[adress] = 1
+        else: self.pageTable[adress] = 0   
         
-    def isStored (self, page):
+        
+    def isStored (self, adress):
+        #if it's in real memory or not
+        pass
+    
         
