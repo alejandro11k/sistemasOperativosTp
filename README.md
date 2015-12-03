a6b244daa1916f09f66e4eca3781c27ab0147294
alejandro11k <alejandro11k@yahoo.com>
2015-12-03 12:57:47 -0300

    readme change and forgotten main add

--- README.md
+++ README.md
@@ -1,31 +1,15 @@
-Pendientes del TP
 
-next:
-Obligatorios
-cola de irqs en interruption manager
-clock con thick a cpu e interruption manager
-hacer correr todo con Threads
-shceduler con estrategia de prioridad
+03/12/2015
+1:50pm
+RC1
 
-Extras?
-una shell con imput? que el imput se ejecute en la simulacion?
-que ps y ls sean programs y se ejecuten en la simulacion?
+SO TP
+UNQ
 
-io:
-ioDeviceCpu
-correr un programa q contenga una instruccion de io (error al buscar la instruccion en memoria?)
+to run this software run main.py in main folder
 
-"un test que deber�an hacer:
-agarr� un programa, una instrucci�n de I/O 
-d�selo al cpu
-cpu.run
-y hac�s un assert que levante la interrupci�n de I/O en la cola de interrupciones" (esto lo dijo el ayudante y lo copi� como pude.
+Grupo:
 
-Correcciones del profe cuando vino a ver nuestro TP
-
-clase.variable para interrupciones
-NO USAR STRINGS
-ponerlo en irq (m�s f�cil para cambiar)
-
-
-
+Victoria Castro Riquelme
+Matias Zacarias
+Alejandro Kronberger
---
+++ main/main.py
@@ -0,0 +1,77 @@
+from hardware.memory import Memory, LimitedMemory
+from hardware.cpu import CPU
+from hardware.hard_disk import HardDisk
+from software.interruption_manager import InterruptionManager
+from software.pcb import PCB
+from software.programs_components.program import Program
+from software.shell import Shell
+from software.pcb_table import PCBTable
+from software.q_ready import QReady
+from software.scheduler import Schedule
+from software.q_io import QIo
+from hardware.io_device import IoDevice
+from software.handlers.handlers import Handlers
+from hardware.clock import Clock
+from time import sleep
+from software.programs_components.programs import Programs
+from software.paging_memory.logical_memory import LogicalMemory
+
+if __name__ == '__main__':
+
+        #hardware
+        #construyo el ordenador
+        hardDisk = HardDisk()
+        #self.memory = Memory()
+        memorySize = 12
+        memory = LimitedMemory(memorySize)
+        cpu = CPU(memory)
+        ioDevice = IoDevice("IOdevice OUT")
+        ioDevice2 = IoDevice("IOdevice IN")
+
+        #kernel
+        qReady = QReady()
+        qIo = QIo()
+        qIo2 = QIo()
+        pcbTable = PCBTable()
+        scheduler = Schedule(qReady,cpu)
+        
+        #test new memory model, need change irqNew to irqNew2 in handler class
+        realMemory = memory
+        pageSize = 4 # FRAME SIZE
+        memory = LogicalMemory(memory,pageSize)
+        
+        interruptionManager = InterruptionManager(cpu,scheduler)
+        cpu.setUp(interruptionManager,memory)
+        
+        ioDevice.setUp(interruptionManager,qIo,memory)
+        ioDevice2.setUp(interruptionManager,qIo2,memory)
+        
+        shell = Shell(interruptionManager,pcbTable,hardDisk)
+        
+        handlers = Handlers(interruptionManager,cpu,
+                                 pcbTable,scheduler,qReady,
+                                 hardDisk,memory,shell)
+        
+        #el handler conoce el device
+        handlers.handlerIOfromCPU.addDevice(ioDevice, qIo)
+        handlers.handlerIOfromCPU.addDevice(ioDevice2, qIo2)
+        
+        #agrego programas al disco
+        programs = Programs()
+        
+        for n in range(len(programs.programs)):
+            hardDisk.save(programs.programs.pop(0))
+        
+        #el device conoce la instruccion
+        
+        ioDevice.learnInstruction(programs.instructions['PRINT'])
+        ioDevice2.learnInstruction(programs.instructions['INPUT'])
+        ioDevice2.learnInstruction(programs.instructions['realINPUT'])
+        
+        ciclos = 2000 #batery
+        clock = Clock(interruptionManager,cpu,ioDevice,ioDevice2,ciclos)
+        
+        shell.load("realINPUT")
+        clock.run()
+       
+
